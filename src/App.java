ad8ec9a38b5479b80282f4e7d5ce1edb48144311
ronaldounifor <ronaldo@unifor.br>
2022-10-27 20:58:58 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,39 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+// MH: 100
+// MA: 10
+// N: 3
+// S: 0
+// HH_0: 50, HA_0: 4
+// HH_1: 30, HA_1: 5
+// HH_2: 25, HA_2: 20
+// B: 4
+// K: 1
+
+// Unit monster = new Unit(MH, MA);
+// Unit[] heros = new Unit[N];
+// int striker = S;
+// for(Line line:heroLines)
+
+
+// roundDamage = 29, 33
+// //roundTank = 105
+// //checar se tem resto
+// int penultimoRound = 100/29 = 3;
+// MH -= penultimoRound*roundDamage;
+
+
+// closestHero = h[0];
+// while(!failed) {
+// 	closestHero.hp -= monster.
+
+
+
+
+
+
+}
+
